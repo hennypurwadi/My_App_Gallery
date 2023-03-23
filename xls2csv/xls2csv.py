6d320abbfc3b05a48f873539e16df40f11e74059
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-23 15:57:32 +0700

    Add files via upload

--- xls2csv/xls2csv.py
+++ xls2csv/xls2csv.py
@@ -3,18 +3,22 @@ import streamlit as st
 import pandas as pd
 from io import BytesIO
 
+# Set page configuration
 st.set_page_config(page_title="Excel to CSV Converter", page_icon=":pencil:")
 
+# Create title
 st.title("Excel to CSV Converter")
 
 # Create file uploader
 uploaded_file = st.file_uploader("Upload an Excel file (.xlsx or .xls)", type=["xlsx", "xls"])
 
+# If file is uploaded
 if uploaded_file is not None:
     # Read in Excel file
     try:
         df = pd.read_excel(uploaded_file)
     except:
+        # Display error message if file cannot be read
         st.error("Failed to read file. Please make sure it is a valid Excel file.")
         st.stop()
 
@@ -22,8 +26,10 @@ if uploaded_file is not None:
     csv = df.to_csv(index=False).encode()
 
     # Create download link
-    b64 = BytesIO(csv).getvalue()
-    href = f'<a href="data:file/csv;base64,{b64.decode()}" download="converted.csv">Download CSV File</a>'
+    with BytesIO() as b:
+        b.write(csv)
+        href = f'<a href="data:file/csv;base64,{b.getvalue().decode()}" download="converted.csv">Download CSV File</a>'
 
-    # Display download link
+    # Display success message and download link
+    st.success("File conversion successful!")
     st.markdown(href, unsafe_allow_html=True)
