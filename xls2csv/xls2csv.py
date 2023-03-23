b70e220131f44ac0a4bf031d632f7b9c525e9e8b
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-23 17:29:22 +0700

    Add files via upload

--- xls2csv/xls2csv.py
+++ xls2csv/xls2csv.py
@@ -21,7 +21,10 @@ if uploaded_file is not None:
     df.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)
 
-    # Create download link
-    b64 = base64.b64encode(csv_buffer.read()).decode()
-    href = f'<a href="data:file/csv;base64,{b64}" download="converted_file.csv">Download CSV File</a>'
-    st.markdown(href, unsafe_allow_html=True)
+    # Create download button
+    st.download_button(
+        label="Download CSV File",
+        data=csv_buffer,
+        file_name="converted_file.csv",
+        mime="text/csv",
+    )
