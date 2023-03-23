9236f838e44d173b1f91a92b74e0e6b01bf2f490
Henny <85436090+hennypurwadi@users.noreply.github.com>
2023-03-23 17:18:27 +0700

    Add files via upload

--- xls2csv/xls2csv.py
+++ xls2csv/xls2csv.py
@@ -9,19 +9,19 @@ st.title("Excel to CSV Converter")
 uploaded_file = st.file_uploader("Upload Excel file", type=["xlsx", "xls"])
 
 if uploaded_file is not None:
-    # Read the Excel file into a DataFrame
+    # Read Excel file into DataFrame
     df = pd.read_excel(uploaded_file)
 
-    # Preview of the data
+    # Preview data
     st.write("Here's a preview of your data:")
     st.write(df.head())
 
-    # Convert the DataFrame to CSV and make it downloadable
+    # Convert DataFrame to CSV
     csv_buffer = BytesIO()
     df.to_csv(csv_buffer, index=False)
     csv_buffer.seek(0)
 
-    # Create a download link
+    # Create download link
     b64 = base64.b64encode(csv_buffer.read()).decode()
     href = f'<a href="data:file/csv;base64,{b64}" download="converted_file.csv">Download CSV File</a>'
     st.markdown(href, unsafe_allow_html=True)
